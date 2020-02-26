15c1b766bfb9c2467fb9dbc0105e668cd41f0692
creatorscan <baskar@fit.vutbr.cz>
2020-02-27 00:23:17 +0100

    minor corrections

--- path.sh
+++ path.sh
@@ -1,7 +1,7 @@
 # changing the project dir will reflect in all files
-export PROJECT_DIR="$HOME/Desktop/sree_proj/project_evo"
-export MODEL_DIR="$PROJECT_DIR/NLP/nnet4_libri_tcp"
+export PROJECT_DIR="$HOME/Desktop/sree_proj/project"
+export MODEL_DIR="$PROJECT_DIR/NLP/nnet3_libri_tcp"
 export KALDI_DIR="$HOME/kaldi"
 
 # defining all binaries and scripts in current environment
-export PATH=$KALDI_DIR/src/online2bin:$PROJECT_DIR/NLP:$PROJECT_DIR/command_synthesiser:$PROJECT_DIR/command_parser:$PROJECT_DIR/evo-sim:$PATH
+export PATH=$KALDI_DIR/src/online2bin:$PROJECT_DIR/NLP:$PROJECT_DIR/command_synthesiser:$PATH
